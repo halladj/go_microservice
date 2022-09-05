6b888e46beb9875937be3900633806a15f21f546
hambz <hambz@arch.localdomain>
2022-09-05 14:40:09 +0100

    generic http handler

--- main.go
+++ main.go
@@ -12,22 +12,23 @@ import (
 
 func main() {
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
-	hh := handlers.NewHello(l)
-	gh := handlers.NewGoodBye(l)
+
+	ph := handlers.NewProducts(l)
 
 	sm := http.NewServeMux()
-	sm.HandleFunc("/", hh.ServeHTTP)
-	sm.HandleFunc("/goodbye", gh.ServeHTTP)
+	sm.Handle("/", ph)
 
 	s := &http.Server{
 		Addr:         ":9090",
 		Handler:      sm,
+		ErrorLog:     l,
 		IdleTimeout:  120 * time.Second,
 		ReadTimeout:  1 * time.Second,
 		WriteTimeout: 1 * time.Second,
 	}
 
 	go func() {
+		l.Println("Starting server on port 9090")
 
 		err := s.ListenAndServe()
 		if err != nil {
