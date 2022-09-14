3fe6a0d35c436c1d2a08fc3271ca3f1f12529c58
hambz <hambz@arch.localdomain>
2022-09-14 16:24:04 +0100

    added gorilla instead of stdlib

--- handlers/products.go
+++ handlers/products.go
@@ -1,11 +1,13 @@
 package handlers
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"practice/data"
-	"regexp"
 	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 type Products struct {
@@ -16,51 +18,7 @@ func NewProducts(l *log.Logger) *Products {
 	return &Products{l}
 }
 
-func (p *Products) ServeHTTP(rw http.ResponseWriter,
-	r *http.Request,
-) {
-
-	if r.Method == http.MethodGet {
-		p.getProducts(rw, r)
-		return
-	}
-
-	if r.Method == http.MethodPost {
-		p.addProducts(rw, r)
-		return
-	}
-
-	if r.Method == http.MethodPut {
-		//Expect the id in the URL
-		reg := regexp.MustCompile(`/([0-9]+)`)
-		g := reg.FindAllStringSubmatch(r.URL.Path, -1)
-
-		if len(g) != 1 {
-			http.Error(rw, "Invalid URI", http.StatusBadRequest)
-			return
-		}
-
-		if len(g[0]) != 2 {
-			http.Error(rw, "Invalid URI", http.StatusBadRequest)
-			return
-		}
-
-		idString := g[0][1]
-		id, err := strconv.Atoi(idString)
-		if err != nil {
-			http.Error(rw, "Invalid URI Atoi", http.StatusBadRequest)
-			return
-		}
-
-		p.updateProducts(id, rw, r)
-		return
-	}
-
-	//Catch all
-	rw.WriteHeader(http.StatusMethodNotAllowed)
-}
-
-func (p *Products) getProducts(rw http.ResponseWriter,
+func (p *Products) GetProducts(rw http.ResponseWriter,
 	r *http.Request,
 ) {
 	p.l.Println("Handle Get Products")
@@ -72,35 +30,30 @@ func (p *Products) getProducts(rw http.ResponseWriter,
 	}
 }
 
-func (p *Products) addProducts(rw http.ResponseWriter,
+func (p *Products) AddProducts(rw http.ResponseWriter,
 	r *http.Request,
 ) {
 
 	p.l.Println("Handle Post Products")
-	prod := &data.Product{}
-	err := prod.FromJSON(r.Body)
+	prod := r.Context().Value(KeyProduct{}).(data.Product)
 
-	if err != nil {
-		http.Error(rw, "Unable to unmarshal json", http.StatusBadRequest)
-	}
-
-	data.AddProduct(prod)
-	p.l.Printf("Prod: %#v", prod)
+	data.AddProduct(&prod)
+	p.l.Printf("Prod: %#v", &prod)
 
 }
 
-func (p *Products) updateProducts(id int, rw http.ResponseWriter,
+func (p Products) UpdateProducts(rw http.ResponseWriter,
 	r *http.Request,
 ) {
-	p.l.Println("Handle PUT Products")
-	prod := &data.Product{}
-	err := prod.FromJSON(r.Body)
-
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
 	if err != nil {
-		http.Error(rw, "Unable to unmarshal json", http.StatusBadRequest)
+		http.Error(rw, "Unable To Convert id", http.StatusBadRequest)
 	}
+	p.l.Println("Handle PUT Products")
+	prod := r.Context().Value(KeyProduct{}).(data.Product)
 
-	err = data.UpdateProfuct(id, prod)
+	err = data.UpdateProfuct(id, &prod)
 	if err == data.ErrorProdcutNotFound {
 		http.Error(rw, "Product Not Found", http.StatusNotFound)
 		return
@@ -110,4 +63,24 @@ func (p *Products) updateProducts(id int, rw http.ResponseWriter,
 		http.Error(rw, "Product Not Found", http.StatusBadRequest)
 		return
 	}
+}
+
+type KeyProduct struct{}
+
+func (p Products) MiddlewareProductValidation(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		prod := data.Product{}
+		err := prod.FromJSON(r.Body)
+
+		if err != nil {
+			http.Error(rw, "Unable to unmarshal json", http.StatusBadRequest)
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), KeyProduct{}, prod)
+		r = r.WithContext(ctx)
+
+		next.ServeHTTP(rw, r)
+
+	})
 }
--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 	"os/signal"
 	"practice/handlers"
 	"time"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
@@ -15,8 +17,18 @@ func main() {
 
 	ph := handlers.NewProducts(l)
 
-	sm := http.NewServeMux()
-	sm.Handle("/", ph)
+	sm := mux.NewRouter()
+
+	getRouter := sm.Methods(http.MethodGet).Subrouter()
+	getRouter.HandleFunc("/", ph.GetProducts)
+
+	putRouter := sm.Methods(http.MethodPut).Subrouter()
+	putRouter.HandleFunc("/{id:[0-9]+}", ph.UpdateProducts)
+	putRouter.Use(ph.MiddlewareProductValidation)
+
+	postRouter := sm.Methods(http.MethodPost).Subrouter()
+	postRouter.HandleFunc("/", ph.AddProducts)
+	postRouter.Use(ph.MiddlewareProductValidation)
 
 	s := &http.Server{
 		Addr:         ":9090",
